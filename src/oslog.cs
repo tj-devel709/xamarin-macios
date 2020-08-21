3749a0431513a9b10e0ed1140c75dbc7a021e33f
TJ Lambert <tjlambert@MININT-C093123.redmond.corp.microsoft.com>
2020-08-21 17:44:25 -0500

    creating oslog.cs

---
+++ src/oslog.cs
@@ -0,0 +1,26 @@
+//
+// OSLog C# bindings
+//
+// Authors:
+//	TJ Lambert <TJ.Lambert@microsoft.com>
+//
+// Copyright (c) Microsoft Corporation.
+//
+
+using System;
+using Foundation;
+using ObjCRuntime;
+
+namespace OSLog {
+
+	
+	// [Unavailable (PlatformName.MacCatalyst)]
+	// [Mac (10,15,4), iOS (13,4)]
+	// [Advice ("This API is not available when using UIKit on macOS.")]
+	// [BaseType (typeof (NSObject))]
+	// interface AEAssessmentConfiguration : NSCopying {
+
+	// }
+
+
+}
